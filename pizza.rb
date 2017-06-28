6fdffafeda1513d1107c77448f2c311d29c92dd7
jkeiffer1 <saros82@hotmail.com>
2017-06-28 14:40:44 -0400

    partial unworking completion of 'pizza' stack error

---
+++ pizza.rb
@@ -0,0 +1,83 @@
+def crust()
+	crust = ("thin","thick","stuffed").choose
+	
+end
+
+def meats()
+	def pepperoni?
+		if 1 == [1,2].sample
+			puts "pepperoni"
+		end
+	end
+
+	def chicken?
+		if 1 == [1,2].sample
+			puts "chicken"
+		end
+	end
+
+	def bacon?
+		if 1 == [1,2].sample
+			puts "bacon"
+		end
+	end
+
+	def steak?
+		if 1 == [1,2].sample
+			puts "steak"
+		end
+	end
+
+	def ham?
+		if 1 == [1,2].sample
+			puts "ham"
+		end
+	end
+
+	def sausage?
+		if 1 == [1,2].sample
+			puts "sausage"
+		end
+	end
+	def anchovie?
+		if 1 == [1,2].sample
+			puts "anchovie"
+		end
+	end
+	meats << perreroni?,chicken?,bacon?,steak?,sausage?,ham?,anchovie?
+end
+
+#def veggies()
+#	onion
+#	peppers (green,banana,jalapeno,roasted red).choose
+#	tomato
+#	olives
+#	spinach
+#	mushrooms
+#	veggies()
+#end
+
+def special()
+	extra meat
+	extra cheese
+	jush cheese
+	extra veggies
+	special()
+end
+ 
+def sauces()
+	marinara
+	ranch
+	alfredo
+	bbq
+	philly
+	sauces()
+end
+
+#def pizza
+	#chicken_bacon_ranch == meats(chicken,bacon) && sauces(bacon)
+	#if meats = chicken,bacon && sauces == ranch
+		#chicken_bacon_ranch
+
+1.times.do |crust|
+	crust		
---
+++ pizzatest.rb
@@ -0,0 +1,46 @@
+
+def meats()
+	def pepperoni?
+		if 1 == [1,2].sample
+			puts "pepperoni"
+		end
+	end
+
+	def chicken?
+		if 1 == [1,2].sample
+			puts "chicken"
+		end
+	end
+
+	def bacon?
+		if 1 == [1,2].sample
+			puts "bacon"
+		end
+	end
+
+	def steak?
+		if 1 == [1,2].sample
+			puts "steak"
+		end
+	end
+
+	def ham?
+		if 1 == [1,2].sample
+			puts "ham"
+		end
+	end
+
+	def sausage?
+		if 1 == [1,2].sample
+			puts "sausage"
+		end
+	end
+	def anchovie?
+		if 1 == [1,2].sample
+			puts "anchovie"
+		end
+	end
+	meats << pepperoni?
+end
+
+meats
