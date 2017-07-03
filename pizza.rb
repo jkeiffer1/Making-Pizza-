f56cf3533795fe232870af349c3fa7b217337a08
jkeiffer1 <saros82@hotmail.com>
2017-07-03 17:32:23 -0400

    Cleaned up unused lines, added size

--- pizza.rb
+++ pizza.rb
@@ -1,16 +1,67 @@
+def pizza
+	puts size
+	puts crust
+	puts sauces
+	meats
+	veggies
+	special
+	puts "$14.99"
+end
+
+def size()
+	x = rand(4)
+	if
+		x == 0
+		x = "personal"
+	elsif
+		x == 1
+		x = "medium"
+	elsif
+		x == 2
+		x = "large"
+	elsif
+		x == 3
+		x = "extra-large"
+	end
+end
+		
+		
+
 def crust()
 	x = rand(3)
 	if
 		x == 0
-		x = "thin"
+		x = "thin crust"
 	elsif
 		x == 1
-		x = "thick"
+		x = "thick crust"
 	elsif
 		x == 2
-		x = "stuffed"
+		x = "stuffed crust"
 	end
 end
+
+
+def sauces()
+	y = rand(5)
+	if
+		y == 0
+		y = "marinara"
+	elsif
+		y == 1
+		y = "ranch"
+	elsif
+		y == 2
+		y = "alfredo"
+	elsif
+		y == 3
+		y = "BBQ"
+	elsif 
+		y == 4
+		y = "philly"
+	end
+end
+
 
 def meats()
 	meats = []
@@ -85,14 +136,14 @@ def veggies()
 	veggies << onion
 
 	def peppers()
-		if 1 == [1,2].sample
+		if 1 == [1,2,3].sample
 			puts "peppers"
 		end
 	end
 	veggies << peppers
 
 	def tomato()
-		if 1 == [1,2,3].sample
+		if 1 == [1,2,3,4].sample
 			puts "tomato"
 		end
 	end
@@ -105,7 +156,7 @@ def veggies()
 	end
 	veggies << olives
 	def spinach()
-		if 1 == [1,2,3].sample
+		if 1 == [1,2,3,4].sample
 			puts "spinach"
 		end
 	end
@@ -129,25 +180,12 @@ def special()
 	end
 end
 
-special()
-#end
- 
-#def sauces()
-#	marinara
-#	ranch
-#	alfredo
-#	bbq
-#	philly
-#	sauces()
-#end
 
-#def pizza
-	#chicken_bacon_ranch == meats(chicken,bacon) && sauces(bacon)
-	#if meats = chicken,bacon && sauces == ranch
-		#chicken_bacon_ranch
+puts "how many pizzas would you like?"
+a = gets.chomp.to_i
 
 
-puts crust
-meats
-veggies
-special
+(a).times do
+puts pizza
+puts " "
+end
